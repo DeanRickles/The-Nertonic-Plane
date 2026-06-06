12bf7c50afe122c24d68358eb1f66aadc17c88d5
Dean Rickles <dean@rickles.co.uk>
2026-06-06 19:41:23 +0000

    removed hide_from_all tag function

--- source/quartz/components/scripts/search.inline.ts
+++ source/quartz/components/scripts/search.inline.ts
@@ -422,7 +422,6 @@ document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
       .filter((item) => {
         const fm = data[item.slug]?.frontmatter
         if (!fm) return true
-        if (fm.hide_from_all === true) return false
         if (fm.hide_from_explorer === true) return false
         return true
       })
